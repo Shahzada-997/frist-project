afecdf7b256dc53072a29475c504a06b126e2475
Shahzada-997 <106319406+Shahzada-997@users.noreply.github.com>
2022-06-02 12:53:28 +0600

    6

---
+++ task 6.cpp
@@ -0,0 +1,32 @@
+/*Two numbers are input through the keyboard into two locations C and D. Write a program to 
+interchange the contents of C and D.*/
+/*
+{
+int c;
+int d;
+printf( " Enter  the  two value:");
+scanf("%d",&c);
+scanf("%d",&d);
+printf ("c=%d",d);
+printf (" d=%d",c);
+
+	
+	
+	
+return 0;	
+}*/#include<stdio.h>
+int main()
+{
+	int c,d;
+	
+	
+	printf("Enter the value of c:");
+	scanf("%d",&c);
+	printf("Enter the value of d:");
+	scanf("%d",&d);
+	printf("c=%d\nd=%d",d,c);
+	
+	return 0;
+}
+	
+
