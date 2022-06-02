82d268aaddba5452bfe90a62a831be9531d3c2d6
Shahzada-997 <106319406+Shahzada-997@users.noreply.github.com>
2022-06-02 12:54:14 +0600

    8

---
+++ task 8.cpp
@@ -0,0 +1,30 @@
+/*If a five-digit number is input through the keyboard, write a program to reverse the number*/
+#include<stdio.h>
+int main()
+{
+	int number,sum,a,b,c,d,e,d1,d2,d3,d4,d5;
+	printf(" Enter the five value :");
+	scanf("%d",number);
+	
+	a=number/10;
+	d5=number%10;
+	b=a/10;
+	d4=a%10;
+	c=b/10;
+	d3=b%10;
+	d=c/10;
+	d2=c%10;
+	e=d/10;
+	d1=d%10;
+	
+	sum =d1=d2+d3+d4+d5;
+	
+	
+printf("Reverse order");
+printf(" %d\t%d\t%d\t%d\t%d\t",d5,d4,d3,d2,d1);
+	
+	
+	
+	
+	return 0;
+}
